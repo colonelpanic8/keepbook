78bb4c2167b3e9405225be41e058f066bc9ba18a
Ivan Malison <IvanMalison@gmail.com>
2026-05-09 17:29:48 -0700

    chore: release v0.3.4

--- flake.nix
+++ flake.nix
@@ -195,9 +195,9 @@
             <key>CFBundlePackageType</key>
             <string>APPL</string>
             <key>CFBundleShortVersionString</key>
-            <string>0.3.3</string>
+            <string>0.3.4</string>
             <key>CFBundleVersion</key>
-            <string>0.3.3</string>
+            <string>0.3.4</string>
             <key>LSMinimumSystemVersion</key>
             <string>13.0</string>
             <key>NSHighResolutionCapable</key>
@@ -626,7 +626,7 @@
         }:
           rustPlatform.buildRustPackage {
             inherit pname buildFeatures buildNoDefaultFeatures postInstall;
-            version = "0.3.3";
+            version = "0.3.4";
             src = cleanSrc;
             cargoLock = {
               lockFile = ./Cargo.lock;
