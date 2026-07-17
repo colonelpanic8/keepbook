a2e2da67b3328accef55be018626b32c7d813f08
Ivan Malison <IvanMalison@gmail.com>
2026-07-17 10:53:36 -0700

    chore: release keepbook 0.7.5

--- flake.nix
+++ flake.nix
@@ -25,8 +25,8 @@
         };
         fenixPkgs = fenix.packages.${system};
         lib = pkgs.lib;
-        appVersion = "0.7.4";
-        androidVersionCode = "700";
+        appVersion = "0.7.5";
+        androidVersionCode = "705";
         appCommit = self.rev or self.dirtyRev or "unknown";
         keepbookDioxusAppId = "org.colonelpanic.keepbook.dioxus";
         keepbookDioxusDesktopAlias = "keepbook-dioxus";
